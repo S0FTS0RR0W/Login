da51a60a9f44c98e729711bdea95d9fa613a8ec4
S0FTS0RR0W <charlie@s0fts0rr0w.com>
2026-01-29 06:08:42 +0000

    first commit

---
+++ app/login/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Button } from '@/components/ui/button';
+
+async function HandleSubmit() {
+    const result = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        }),
+    });
+}
+
+export default function loginPage() {
+    return (
+        <div>
+            <h1 className="flex font-mono text-2xl text-shadow-lime-50">
+                Login
+            </h1>
+        
+        <form className="rounded-2xl border-white items-center justify-center flex flex-col">
+            <input className="" type="username" placeholder="Username" />
+            <input className="" type="password" placeholder="Password" />
+            <Button onClick={HandleSubmit}>Login</Button>
+        </form>
+        
+        </div>
+    )
+}
